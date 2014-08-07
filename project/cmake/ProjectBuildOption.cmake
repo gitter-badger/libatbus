fda204cdc3b670dc0e25ba16a9267dc90d9855d4
owt5008137 <owt5008137@live.com>
2014-08-07 19:46:50 +0800

    add first protocol

--- project/cmake/ProjectBuildOption.cmake
+++ project/cmake/ProjectBuildOption.cmake
@@ -2,6 +2,9 @@
 #####################################################################
 option(BUILD_SHARED_LIBS "Build shared libraries (DLLs)." OFF)
 
+# atbus 选项
+set(ATBUS_MACRO_BUSID_TYPE "uint64_t" CACHE STRING "busid type of atbus")
+
 # 测试配置选项
 set(GTEST_ROOT "" CACHE STRING "GTest root directory")
 set(BOOST_ROOT "" CACHE STRING "Boost root directory")
